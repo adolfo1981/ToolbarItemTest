ee4c91214f5c764788c7a5b0bc7794956eff047a
Adolfo <adolfo1205@hotmail.com>
2021-03-22 09:40:33 -0500

    Add project files.

---
+++ ToolbarItemTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ToolbarItemTest.ViewModels;
+using Xamarin.Forms;
+
+namespace ToolbarItemTest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
